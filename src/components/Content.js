fc19c0bb073b1155901510a26aa23025b2a309dc
raghueswar55 <raghueswar55@gmail.com>
2020-07-20 11:21:44 +0530

    Refactored font-size letter-spacing.

--- src/components/Content.js
+++ src/components/Content.js
@@ -17,19 +17,19 @@ class Content extends Component {
                     button:<button>SIGN UP FOR FREE</button>
                 },
                 {   
-                    title:"Free",
-                    price:0,
-                    usersIncluded:10,
-                    storage:2,
+                    title:"Pro",
+                    price:15,
+                    usersIncluded:20,
+                    storage:10,
                     support:"Priority email support",
                     isPopular:true,
                     button:<button>GET STARTED</button>
                 },
                 {   
-                    title:"Free",
-                    price:0,
-                    usersIncluded:10,
-                    storage:2,
+                    title:"Enterprise",
+                    price:30,
+                    usersIncluded:50,
+                    storage:30,
                     support:"Phone & email support",
                     isPopular:false,
                     button:<button>CONTACT US</button>
