7237392957ee6c0667b1a822e6baf42eb730c983
raghueswar55 <raghueswar55@gmail.com>
2020-07-22 09:46:24 +0530

    Refactored profile UI.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,25 +8,33 @@ class Profile extends Component {
         this.state = {user:null}    
     }
 
-    UNSAFE_componentWillMount() {
+    componentDidMount() {
         fetch(`https://reqres.in/api/users/${window.atob(this.props.match.params.id)-0}`)
         .then(res => res.json())
         .then(data => this.setState({user:data}));
     }
 
+    componentDidUpdate() {
+        this.autoSize()
+    }
+
+    autoSize() {
+        this.refs["about"].style.height = `${this.refs["about"].scrollHeight+5}px`
+    }
+
     render() {
         if (this.state.user == null){
-            return <div className="loader"></div>
+            return <div id="profile"><div className="loader"></div></div>
         }
         return (
             <div id="profile">
-                {console.log(this.state.user)}
                 <div id="profile-data">
-                    <div><span>email</span><input value={this.state.user.data.email} className="blocked-input-field"></input></div>
-                    <div><span>first name</span><input value={this.state.user.data.first_name} className="blocked-input-field"></input></div>
-                    <div><span>last name</span><input value={this.state.user.data.last_name} className="blocked-input-field"></input></div>
-                    <div><span>company</span><input value={this.state.user.ad.company} className="blocked-input-field"></input></div>
-                    <div><span>about</span><textarea value={this.state.user.ad.text} className="blocked-input-field"></textarea></div>
+                    <div id="profile-data-image"><img src={this.state.user.data.avatar} alt=""/></div>
+                    <div id="profile-data-mail"><span>email</span><span>:</span><input defaultValue={this.state.user.data.email} className="blocked-input-field"></input></div>
+                    <div id="profile-data-first-name"><span>first name</span><span>:</span><input defaultValue={this.state.user.data.first_name} className="blocked-input-field"></input></div>
+                    <div id="profile-data-last-name"><span>last name</span><span>:</span><input defaultValue={this.state.user.data.last_name} className="blocked-input-field"></input></div>
+                    <div id="profile-data-company-name"><span>company</span><span>:</span><input defaultValue={this.state.user.ad.company} className="blocked-input-field"></input></div>
+                    <div id="profile-data-about"><span>about</span><span>:</span><textarea ref="about" defaultValue={this.state.user.ad.text} className="blocked-input-field"></textarea></div>
                 </div>
             </div>
         );
